72e313841699a43bb16d251504f8bc51280f0f9b
Immanuel Haffner <immanuel.haffner@uni-saarland.de>
2017-01-23 13:44:28 +0100

    Improved mouse clicks on tasklist.

--- rc.lua
+++ rc.lua
@@ -317,7 +317,28 @@ local taglist_buttons = awful.util.table.join(
 
 local tasklist_buttons = awful.util.table.join(
                      awful.button({ }, 1, function (c)
-                                              if c == client.focus then
+                       if c.minimized then
+                         c.minimized = false
+                         if not c:isvisible() and c.first_tag then
+                           c.first_tag:view_only()
+                         end
+                         client.focus = c
+                         c:raise()
+                       elseif client.focus == c then
+                         if c.maximized then
+                           c.maximized_horizontal = false
+                           c.maximized_vertical = false
+                         else
+                           c.maximized_horizontal = true
+                           c.maximized_vertical = true
+                           c:raise()
+                         end
+                       else
+                         client.focus = c
+                       end
+                     end),
+                     awful.button({ }, 3, function (c)
+                                              if not c.minimized then
                                                   c.minimized = true
                                               else
                                                   -- Without this, the following
@@ -326,13 +347,9 @@ local tasklist_buttons = awful.util.table.join(
                                                   if not c:isvisible() and c.first_tag then
                                                       c.first_tag:view_only()
                                                   end
-                                                  -- This will also un-minimize
-                                                  -- the client, if needed
-                                                  client.focus = c
-                                                  c:raise()
                                               end
                                           end),
-                     awful.button({ }, 3, client_menu_toggle_fn()),
+                     awful.button({ }, 2, client_menu_toggle_fn()),
                      awful.button({ }, 4, function ()
                                               awful.client.focus.byidx(1)
                                           end),
