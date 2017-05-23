b1b2706362e2c1f898545b265eb1768bde72efe7
Immanuel Haffner <haffner.immanuel@gmail.com>
2017-05-23 10:28:30 +0200

    Don't autofocus new windows.

--- rc.lua
+++ rc.lua
@@ -894,7 +894,7 @@ awful.rules.rules = {
     { rule = { },
       properties = { border_width = beautiful.border_width,
                      border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
+                     --focus = awful.client.focus.filter,
                      raise = true,
                      keys = clientkeys,
                      buttons = clientbuttons,
