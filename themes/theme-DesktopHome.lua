480b6cf6aceef714c061bb6438dd2eb8f6d47dae
Immanuel Haffner <haffner.immanuel@gmail.com>
2025-01-17 13:55:46 +0100

    Set city code for SB

--- themes/theme-DesktopHome.lua
+++ themes/theme-DesktopHome.lua
@@ -114,8 +114,8 @@ theme.titlebar_maximized_button_focus_inactive  = theme.dir .. "/icons/titlebar/
 theme.titlebar_maximized_button_normal_inactive = theme.dir .. "/icons/titlebar/maximized_normal_inactive.png"
 
 -- Custom config
--- theme.weather_city_id                           = 2842632 -- Saarlouis
-theme.weather_city_id                           = 2842647 -- Saarbrücken
+theme.weather_appid                             = '643ca014fdd2fccaf1a470e52d9002bd'  -- API key
+theme.weather_city_zip_code                     = '66117,DE'  -- Saarbrücken
 theme.quake_height_relative                     = .35 -- relative
 theme.mem_load_lo                               =  2000 -- in MiB
 theme.mem_load_hi                               = 26000 -- in MiB
